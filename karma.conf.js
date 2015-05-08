bc8d09ee9468e58f1165dc1727596e12074eb21c
Sanjeya Cooray <sanjeya.cooray@gmail.com>
2015-05-08 22:12:18 +0200

    fix: test angular paths

--- karma.conf.js
+++ karma.conf.js
@@ -12,8 +12,8 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            './src/bower_components/angular/angular.min.js',
-            './src/bower_components/angular-route/angular-route.min.js',
+            './bower_components/angular/angular.min.js',
+            './bower_components/angular-route/angular-route.min.js',
             './node_modules/angular-mocks/angular-mocks.js',            
             './src/scripts/**/*.js'
         ],
